c4b4c8116b720864ec09f3ccc28bf2a7bc21829a
Julian Lettner <julian.lettner@gmail.com>
2015-01-23 20:25:07 -0800

    Adapt script for program 1 grading.

--- runquiz.py
+++ runquiz.py
@@ -5,12 +5,22 @@ from subprocess import call
 # Configure the following things before you start grading!
 interactive = True
 # Where are the submissions located?
-labdir = '/home/yln/work/ics46-grading/quiz1/quiz1submissions/Lab 2/'
+labdir = '/home/yln/work/ics46-grading/program1/program1submissions/Lab 1'
 # On Windows, also add the binary libraries here
-template_files = ['/home/yln/work/ics46-grading/quiz1/q1helper/src/test_quiz1.cpp']
+template_files = [
+    '/home/yln/work/ics46-grading/program1/graph1.txt',
+    '/home/yln/work/ics46-grading/program1/graph2.txt',
+    '/home/yln/work/ics46-grading/program1/graph3.txt',
+    '/home/yln/work/ics46-grading/program1/input1',
+    '/home/yln/work/ics46-grading/program1/input2',
+    '/home/yln/work/ics46-grading/program1/input3',
+    '/home/yln/work/ics46-grading/program1/expected_output1',
+    '/home/yln/work/ics46-grading/program1/expected_output2',
+    '/home/yln/work/ics46-grading/program1/expected_output3'
+]
 # compilation command (see what Eclipse does to get an idea)
-executable = 'test_quiz1'
-gcc_comand_line = 'g++ -std=c++11 -O0 -g3 -I/home/yln/work/ics46/courselib/src -I/home/yln/work/ics46/googletestlib/include -L/home/yln/work/ics46/courselib/Debug -L/home/yln/work/ics46/googletestlib/make -o test_quiz1 test_quiz1.cpp -lgtest -lcourselib'
+executable = 'reachable'
+gcc_comand_line = 'g++ -std=c++11 -O0 -g3 -I/home/yln/work/ics46/courselib/src -I/home/yln/work/ics46/googletestlib/include -L/home/yln/work/ics46/courselib/Debug -L/home/yln/work/ics46/googletestlib/make -o reachable reachable.cpp -lgtest -lcourselib'
 
 
 def copy_template_files(submission_dir):
@@ -25,6 +35,22 @@ def compile_program(submission_dir):
     # set working directory
     os.chdir(submission_dir)
     return call(gcc_comand_line.split(' '))
+
+
+def run(submission_dir):
+    # set working directory
+    os.chdir(submission_dir)
+    return os.system("./reachable  < input1  > output1") \
+        or os.system("./reachable  < input2  > output2") \
+        or os.system("./reachable  < input3  > output3")
+
+
+def diff(submission_dir):
+    # set working directory
+    os.chdir(submission_dir)
+    os.system("git diff output1 expected_output1")
+    os.system("git diff output2 expected_output2")
+    os.system("git diff output3 expected_output3")
 
 
 def test(submission_dir):
@@ -46,16 +72,24 @@ def run_quiz(name, submission_dir):
         print('Compilation failed!')
         return 1
     print('Compiled')
-    if test(submission_dir) != 0:
-        print('Testing (ITSELF) failed!')
+    if run(submission_dir) != 0:
+        print('error while running')
         return 1
+    diff(submission_dir)
+
+    # if test(submission_dir) != 0:
+    #     print('Testing (ITSELF) failed!')
+    #     return 1
     print('\nFinished for: ' + name)
+    proc = input('Mark as processed? [ENTER]: yes, [n]: no: ')
+    if not proc.startswith('n'):
+        open('PROCESSED', 'w+')
     return 0
 
 
 def main():
     submissions = [(name, os.path.join(labdir, name)) for name in sorted(os.listdir(labdir))
-                   if os.path.isdir(os.path.join(labdir, name))]
+                   if os.path.isdir(os.path.join(labdir, name)) and not os.path.isfile(os.path.join(labdir, name, "PROCESSED"))]
 
     errors = 0
     for (n, d) in submissions:
