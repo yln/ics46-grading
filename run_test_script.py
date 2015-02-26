8cae8a61e059cb2ebe91d23a52163f378256d2f2
Julian Lettner <julian.lettner@gmail.com>
2015-02-25 22:38:46 -0800

    Adapt script for program 3 (and even fixed a small bug).

--- run_test_script.py
+++ run_test_script.py
@@ -1,28 +1,8 @@
 import os
 import subprocess
 
-labdir = '/home/yln/work/ics46-grading/program2/program2submissions/Lab 2'
+labdir = '/home/yln/work/ics46-grading/program3/program3submissions/Lab 2'
 tests = [
-    [
-        './test_queue --gtest_filter=QueueTest.empty',
-        './test_queue --gtest_filter=QueueTest.size',
-        './test_queue --gtest_filter=QueueTest.peek',
-        './test_queue --gtest_filter=QueueTest.enqueue',
-        './test_queue --gtest_filter=QueueTest.operator_eq_ne',
-        './test_queue --gtest_filter=QueueTest.operator_stream_insert',
-        './test_queue --gtest_filter=QueueTest.enqueue_iterator_param',
-        './test_queue --gtest_filter=QueueTest.clear',
-        './test_queue --gtest_filter=QueueTest.dequeue1',
-        './test_queue --gtest_filter=QueueTest.dequeue2',
-        './test_queue --gtest_filter=QueueTest.assignment',
-        './test_queue --gtest_filter=QueueTest.iterator_plusplus',
-        './test_queue --gtest_filter=QueueTest.iterator_simple',
-        './test_queue --gtest_filter=QueueTest.iterator_erase',
-        './test_queue --gtest_filter=QueueTest.iterator_exception_concurrent_modification_error',
-        './test_queue --gtest_filter=QueueTest.constructors',
-        './test_queue --gtest_filter=QueueTest.large_scale',
-        './test_queue --gtest_filter=QueueTest.large_scale_speed'
-    ],
     [
         './test_priority_queue --gtest_filter=PriorityQueueTest.empty',
         './test_priority_queue --gtest_filter=PriorityQueueTest.size',
@@ -35,42 +15,41 @@ tests = [
         './test_priority_queue --gtest_filter=PriorityQueueTest.dequeue1',
         './test_priority_queue --gtest_filter=PriorityQueueTest.dequeue2',
         './test_priority_queue --gtest_filter=PriorityQueueTest.assignment',
-        './test_priority_queue --gtest_filter=PriorityQueueTest.iterator_plusplus',
         './test_priority_queue --gtest_filter=PriorityQueueTest.iterator_simple',
         './test_priority_queue --gtest_filter=PriorityQueueTest.iterator_erase',
+        './test_priority_queue --gtest_filter=PriorityQueueTest.iterator_erase_heap_special_case',
         './test_priority_queue --gtest_filter=PriorityQueueTest.iterator_exception_concurrent_modification_error',
         './test_priority_queue --gtest_filter=PriorityQueueTest.constructors',
         './test_priority_queue --gtest_filter=PriorityQueueTest.large_scale',
-        './test_priority_queue --gtest_filter=PriorityQueueTest.large_scale_speed'
+        './test_priority_queue --gtest_filter=PriorityQueueTest.large_scale_speed1',
+        './test_priority_queue --gtest_filter=PriorityQueueTest.large_scale_speed2',
     ],
     [
-        './test_set --gtest_filter=SetTest.empty',
-        './test_set --gtest_filter=SetTest.size',
-        './test_set --gtest_filter=SetTest.contains',
-        './test_set --gtest_filter=SetTest.insert',
-        './test_set --gtest_filter=SetTest.operator_rel',
-        './test_set --gtest_filter=SetTest.operator_stream_insert',
-        './test_set --gtest_filter=SetTest.insert_iterator_param',
-        './test_set --gtest_filter=SetTest.contains_iterator_param',
-        './test_set --gtest_filter=SetTest.clear',
-        './test_set --gtest_filter=SetTest.erase',
-        './test_set --gtest_filter=SetTest.erase_iterator_param',
-        './test_set --gtest_filter=SetTest.retains_iterator_param',
-        './test_set --gtest_filter=SetTest.assignment',
-        './test_set --gtest_filter=SetTest.iterator_plusplus',
-        './test_set --gtest_filter=SetTest.iterator_simple',
-        './test_set --gtest_filter=SetTest.iterator_erase',
-        './test_set --gtest_filter=SetTest.iterator_exception_concurrent_modification_error',
-        './test_set --gtest_filter=SetTest.constructors',
-        './test_set --gtest_filter=SetTest.large_scale',
-        './test_set --gtest_filter=SetTest.large_scale_speed'
+        './test_map --gtest_filter=MapTest.empty',
+        './test_map --gtest_filter=MapTest.size',
+        './test_map --gtest_filter=MapTest.has_key',
+        './test_map --gtest_filter=MapTest.has_value',
+        './test_map --gtest_filter=MapTest.put',
+        './test_map --gtest_filter=MapTest.put_index',
+        './test_map --gtest_filter=MapTest.operator_rel',
+        './test_map --gtest_filter=MapTest.operator_stream_insert',
+        './test_map --gtest_filter=MapTest.insert_iterator_param',
+        './test_map --gtest_filter=MapTest.clear',
+        './test_map --gtest_filter=MapTest.erase',
+        './test_map --gtest_filter=MapTest.assignment',
+        './test_map --gtest_filter=MapTest.iterator_plusplus',
+        './test_map --gtest_filter=MapTest.iterator_simple',
+        './test_map --gtest_filter=MapTest.iterator_erase',
+        './test_map --gtest_filter=MapTest.iterator_exception_concurrent_modification_error',
+        './test_map --gtest_filter=MapTest.constructors',
+        './test_map --gtest_filter=MapTest.large_scale',
+        './test_map --gtest_filter=MapTest.large_scale_speed'
     ]
 ]
 
 gcc_comand_lines = [
-    ('queue', 'g++ -std=c++11 -O0 -g3 -I/home/yln/work/ics46/courselib/src -I/home/yln/work/ics46/googletestlib/include -L/home/yln/work/ics46/courselib/Debug -L/home/yln/work/ics46/googletestlib/make -o test_queue test_queue.cpp -lgtest -lcourselib'),
-    ('priority q', 'g++ -std=c++11 -O0 -g3 -I/home/yln/work/ics46/courselib/src -I/home/yln/work/ics46/googletestlib/include -L/home/yln/work/ics46/courselib/Debug -L/home/yln/work/ics46/googletestlib/make -o test_priority_queue test_priority_queue.cpp -lgtest -lcourselib'),
-    ('set', 'g++ -std=c++11 -O0 -g3 -I/home/yln/work/ics46/courselib/src -I/home/yln/work/ics46/googletestlib/include -L/home/yln/work/ics46/courselib/Debug -L/home/yln/work/ics46/googletestlib/make -o test_set test_set.cpp -lgtest -lcourselib')
+    ('heap', 'g++ -std=c++11 -O0 -g3 -I/home/yln/work/ics46/courselib/src -I/home/yln/work/ics46/googletestlib/include -L/home/yln/work/ics46/courselib/Debug -L/home/yln/work/ics46/googletestlib/make -o test_priority_queue test_priority_queue.cpp -lgtest -lcourselib'),
+    ('hashmap', 'g++ -std=c++11 -O0 -g3 -I/home/yln/work/ics46/courselib/src -I/home/yln/work/ics46/googletestlib/include -L/home/yln/work/ics46/courselib/Debug -L/home/yln/work/ics46/googletestlib/make -o test_map test_map.cpp -lgtest -lcourselib'),
 ]
 
 
@@ -91,8 +70,8 @@ def main():
             if retval != 0:
                 print('Could not compile', cc[0])
         # execute tests
-        large_fail = False
         for testset in tests:
+            large_fail = False
             passing = 0
             for t in testset:
                 res = -1
